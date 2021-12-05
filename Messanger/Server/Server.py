81ae1fabdc47e765b3a6f081b474f9043b37a209
Alexandr Sukhorukov <cyxopykhow@gmail.com>
2021-12-05 21:11:33 +0700

    working db connection

--- Messanger/Server/Server.py
+++ Messanger/Server/Server.py
@@ -1,8 +1,12 @@
 from aiohttp import web
-import asyncpg
+from asyncpg import connection
 
+import psycopg2
+import asyncio
 
 routes = web.RouteTableDef()
+
+
 
 
 @routes.get('/')
@@ -12,15 +16,28 @@ async def root(_) -> web.Response:
 
 
 
+def connect_to_db():
+    try:
+        conn = psycopg2.connect( host="localhost",
+        database="Test",
+        user="postgres",
+        password='Tumbochka1' )
 
-
-
-
-
+        cur = conn.cursor()
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+       
 
 
 
 if __name__ == '__main__':
+    
+    connect_to_db()
+
+
     app = web.Application()
     app.add_routes(routes)
     web.run_app(app,host ='localhost',port=3333)
