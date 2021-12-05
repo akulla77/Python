5e3b4bd3ecabdcb72560ba86966e2a5b5d3518be
Alexandr Sukhorukov <cyxopykhow@gmail.com>
2021-12-05 15:16:59 +0700

    skel of web server

--- Messanger/Server/Server.py
+++ Messanger/Server/Server.py
@@ -1,29 +1,12 @@
-import asyncio
-import json
-import logging
+from aiohttp import web
 
-from abc import abstractmethod
-from asyncio.streams import StreamReader, StreamWriter
+
+routes = web.RouteTableDef()
 
 
 
-
-
-
-async def main():
-    # processor = CommandProcessor(MemoryStorage())
-
-    # запускаем сервер на localhost:3333
-    server = await asyncio.start_server('localhost', 3333)
-    logging.info('Server started')
-
-    async with server:
-        await server.serve_forever()
 
 if __name__ == '__main__':
-    # Logger.configure_logger('tcp_server_example')
-
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        logging.info('Server stopped')
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app)
