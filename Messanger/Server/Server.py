eed279a3d12e46a112db517a7582b48965a0e77f
Alexandr Sukhorukov <cyxopykhow@gmail.com>
2021-12-05 15:22:39 +0700

    workin web server with 1 route

--- Messanger/Server/Server.py
+++ Messanger/Server/Server.py
@@ -4,9 +4,13 @@ from aiohttp import web
 routes = web.RouteTableDef()
 
 
+@routes.get('/')
+async def root(_) -> web.Response:
+    print(web.HTTPOk)
+    raise web.HTTPOk
 
 
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes(routes)
-    web.run_app(app)
+    web.run_app(app,host ='localhost',port=3333)
